ba4b2ece20f55119939462510547d75ff87ac4fd
Maria Wieckowska <mkw98@gazeta.pl>
2018-12-16 11:37:47 +0100

    fix problem with stars

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,5 @@
 (function () {
-  const allStars = document.querySelectorAll(".stars.star");
-  //const allStars = document.getElementsByClassName("stars star");
-  //const allStars = document.querySelectorAll(".star.stars");
+  const allStars = document.querySelectorAll(".stars");
 
   for (let i = 0; i < allStars.length; i++) {
     allStars[i].addEventListener("click", rate);
@@ -10,40 +8,44 @@
   }
   function rate(event) {
     const clickedStar = event.target;
-    const stars = this.querySelectorAll(".star");
-    let rated = false;
-    stars.forEach(function(star) {
-      if (rated) {
-        star.classList.remove("full");
-        star.classList.remove("client-star");
-      } else {
-        star.classList.add("full");
-        star.classList.add("client-star");
-      }
-      if (star === clickedStar) {
-        rated = true;
-      }
-    });
+    if (clickedStar.classList.contains("star")) {
+      const stars = this.querySelectorAll(".star");
+      let rated = false;
+      stars.forEach(function(star) {
+        if (rated) {
+          star.classList.remove("full");
+          star.classList.remove("client-star");
+        } else {
+          star.classList.add("full");
+          star.classList.add("client-star");
+        }
+        if (star === clickedStar) {
+          rated = true;
+        }
+      });
+    }
   }
   function hover(event) {
     const clientStar = event.target;
-    const stars = this.querySelectorAll(".star");
-    let isHover = false;
-    stars.forEach(function(star) {
-      if (isHover) {
+    if (clientStar.classList.contains("star")) {
+      const stars = this.querySelectorAll(".star");
+      let isHover = false;
+      stars.forEach(function(star) {
+        if (isHover) {
         star.classList.remove("star-hover");
-      } else {
+        } else {
         star.classList.add("star-hover");
-      }
-      if (star === clientStar) {
+        }
+        if (star === clientStar) {
         isHover = true;
-      }
-    });
+        }
+      });
+    }
   }
   function noHover(event) {
-    const stars = this.querySelectorAll(".star");
+  	const stars = this.querySelectorAll(".star");
     stars.forEach(function(star) {
-    star.classList.remove("star-hover");
+      star.classList.remove("star-hover");
     });
   }
 })();
